6f7eef3788e81b48217edd367b3ee4555adc0b48
Code With Deep <deeppoptani777@gmail.com>
2026-02-07 20:27:46 +0530

    Create FindOcc.java

---
+++ Recursion2/FindOcc.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class FindOcc
+{
+    public static int first = -1;
+    public static int last = -1;
+    
+    public static void findOcc(String str, int idx, char element){
+        if(idx == str.length()){      
+            System.out.println("First Occurrence: " + first);
+            System.out.println("Last Occurrence: " + last);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+
+        if(currChar == element){
+            if(first == -1){
+                first = idx;
+            }else{
+                last = idx;
+            }
+        }
+
+        findOcc(str, idx+1, element);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String str = sc.nextLine();
+        
+        findOcc(str, 0, 'a');
+    }
+}
