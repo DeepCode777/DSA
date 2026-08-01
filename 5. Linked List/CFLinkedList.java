b238010cbcb2465969d10a76af1c2989ee085049
Code With Deep <deeppoptani777@gmail.com>
2026-08-01 23:14:39 +0530

    Update CFLinkedList.java

--- 5. Linked List/CFLinkedList.java
+++ 5. Linked List/CFLinkedList.java
@@ -1,3 +1,5 @@
+// Collection Framework Library (Linked List)
+
 import java.util.LinkedList;
 import java.util.Collections;
 
@@ -5,20 +7,25 @@ public class CFLinkedList{
     public static void main(String[] args){
         LinkedList<Integer> list = new LinkedList<>();
         
+        // add  front of LL
         list.add(1);
         list.add(2);
         list.addFirst(0);
         System.out.println(list);
 
+        // Add at last In LL
         list.addLast(5);
         System.out.println(list);
-        
+
+        // remove from Link list 
         list.removeLast();
         System.out.println(list);
-        
+
+        // remove from first
         list.removeFirst();
         System.out.println(list);
-        
+
+        // revers
         Collections.reverse(list);
         System.out.println(list);
     }
