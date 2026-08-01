000c722dadb7479bc31922610748215208db2fc4
Code With Deep <deeppoptani777@gmail.com>
2026-08-01 23:05:31 +0530

    Prime number

---
+++ Recursion 1/1_PrintNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Program
+{
+    public static void printNumber(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.println(n);
+        printNumber(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        printNumber(n);
+    }
+}
